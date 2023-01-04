71b9383a49a2b4db789eea185ce01eb843ce7131
Daniel Szemerey <account@szemerey.eu>
2023-01-04 15:38:33 +0100

    feature(Saving): Savemodes can be passed in as a string now.

--- src/krisi/examples/basic_compare.py
+++ src/krisi/examples/basic_compare.py
@@ -5,4 +5,4 @@ from krisi import compare, evaluate
 scorecards = [
     evaluate(y=np.random.rand(1000), predictions=np.random.rand(1000)) for _ in range(5)
 ]
-compare(scorecards, metrics_to_display=["mse", "mae"])
+compare(scorecards, sort_metric_key="rmse", metrics_to_display=["mse", "mae"])
