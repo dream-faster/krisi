05a414a47d3dd60218de55798ca32a978a9ed761
Daniel Szemerey <account@szemerey.eu>
2023-01-04 15:31:34 +0100

    feature(Printing): Sorted values are in bold now.

--- src/krisi/examples/basic_compare.py
+++ src/krisi/examples/basic_compare.py
@@ -6,4 +6,4 @@ from krisi.evaluate.compare import compare
 scorecards = [
     evaluate(y=np.random.rand(1000), predictions=np.random.rand(1000)) for _ in range(5)
 ]
-compare(scorecards)
+compare(scorecards, metrics_to_display=["mse", "mae"])
