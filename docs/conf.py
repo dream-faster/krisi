6498be04bc598105f00eb3dcaf9982eaab11db00
Daniel Szemerey <account@szemerey.eu>
2023-01-05 23:06:04 +0100

    feature(Docs): Sphinx now has ``autodoc``

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.duration"]
+extensions = ["sphinx.ext.duration", "sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
