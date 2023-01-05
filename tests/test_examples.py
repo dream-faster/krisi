82ec4c709641d6ab24937bdee99e100c5d5a9573
Daniel Szemerey <account@szemerey.eu>
2023-01-05 20:18:43 +0100

    feature(Tests): Example integration tests added. (#56)

---
+++ tests/test_examples.py
@@ -0,0 +1,34 @@
+def test_basic_eda():
+    import krisi.examples.basic_eda
+
+
+def test_basic_loading():
+    import krisi.examples.basic_loading
+
+
+def test_basic_pdf_report():
+    import krisi.examples.basic_pdf_report
+
+
+def test_basic_saving():
+    import krisi.examples.basic_saving
+
+
+def test_basic_scorecard():
+    import krisi.examples.basic_scorecard
+
+
+def test_compare_classification():
+    import krisi.examples.compare_classification
+
+
+def test_compare_regression():
+    import krisi.examples.compare_regression
+
+
+def test_minimal_classification():
+    import krisi.examples.minimal_classification
+
+
+def test_minimal_regression():
+    import krisi.examples.minimal_regression
