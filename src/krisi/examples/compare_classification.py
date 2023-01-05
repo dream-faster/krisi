0cec99b64dc9f48dd4f0446a169d4598413cb07b
Daniel Szemerey <account@szemerey.eu>
2023-01-05 17:12:29 +0100

    feature(Example): Added example comparison for regression. Renamed examples.

---
+++ src/krisi/examples/compare_classification.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from krisi import compare, evaluate
+
+scorecards = [
+    evaluate(y=np.random.randint(0, 2, 1000), predictions=np.random.randint(0, 2, 1000))
+    for _ in range(5)
+]
+compare(
+    scorecards, sort_metric_key="accuracy", metrics_to_display=["recall", "f1_score"]
+)
