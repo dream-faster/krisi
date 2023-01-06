5409034051d027354156cd4ed1a679025e2a7921
Daniel Szemerey <account@szemerey.eu>
2023-01-06 17:05:17 +0100

    fix: Path

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath("../krisi"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
