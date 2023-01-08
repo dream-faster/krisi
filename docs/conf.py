23b99db0ea077113502a9e6716aeefe445288b9c
Daniel Szemerey <account@szemerey.eu>
2023-01-08 19:38:57 +0100

    fix: Maybe first installing the package works?

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(1, os.path.abspath(".."))
-sys.path.insert(2, os.path.abspath("./src"))
+# sys.path.insert(1, os.path.abspath(".."))
+# sys.path.insert(2, os.path.abspath("./src"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
