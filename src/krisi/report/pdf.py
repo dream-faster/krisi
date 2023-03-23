3883864e28e47f29864ee5348fe9f8c04bed01a5
Daniel Szemerey <account@szemerey.eu>
2023-03-23 18:08:48 +0100

    fix: binary.

--- src/krisi/report/pdf.py
+++ src/krisi/report/pdf.py
@@ -45,7 +45,7 @@ def __convert_html_to_pdf(
     css = CSS(string=css_string)
     pdf = HTML(string=source_html).write_pdf(stylesheets=[css])
 
-    open(f"{output_path}/{report_name}", "w").write(pdf)
+    open(f"{output_path}/{report_name}", "wb").write(pdf)
 
 
 def convert_figures_to_html(figures: List["go.Figure"]) -> str:
