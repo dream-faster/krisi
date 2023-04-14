5e37292eaaf7201f533b68b55a60f1ad9ad6c083
Daniel Szemerey <account@szemerey.eu>
2023-04-14 18:50:34 +0200

    feature(Report): Comparing now possible as a dataframe.

--- src/krisi/evaluate/compare.py
+++ src/krisi/evaluate/compare.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import pandas as pd
+
 from krisi.evaluate.scorecard import ScoreCard
 from krisi.utils.printing import bold
 
@@ -8,19 +10,33 @@ def compare(
     scorecards: List[ScoreCard],
     sort_metric_key: str = "rmse",
     metrics_to_display: List[str] = [],
+    dataframe: bool = True,
 ) -> None:
-
-    metric_title = "".join([f"{metric:<15s}" for metric in metrics_to_display])
-    print(
-        f"{'model_name':>30s}    {bold(f'{sort_metric_key:<15s}', rich=False)} {metric_title}"
-    )
     scorecards.sort(reverse=True, key=lambda x: x[sort_metric_key].result)
-
-    for scorecard in scorecards:
-        metrics = "".join(
-            [f"{scorecard[metric].result:<15.5}" for metric in metrics_to_display]
+    if dataframe:
+        df = pd.concat(
+            [
+                pd.Series(
+                    [scorecard[metric].result for scorecard in scorecards],
+                    name=metric,
+                    index=[scorecard.metadata.model_name for scorecard in scorecards],
+                )
+                for metric in metrics_to_display
+            ],
+            axis="columns",
         )
+        print(df)
 
+    else:
+        metric_title = "".join([f"{metric:<15s}" for metric in metrics_to_display])
         print(
-            f"{scorecard.metadata.model_name:>30s}    {bold(f'{scorecard[sort_metric_key].result:<15.5}', rich=False)} {metrics}"
+            f"{'model_name':>30s}    {bold(f'{sort_metric_key:<15s}', rich=False)} {metric_title}"
         )
+        for scorecard in scorecards:
+            metrics = "".join(
+                [f"{scorecard[metric].result:<15.5}" for metric in metrics_to_display]
+            )
+
+            print(
+                f"{scorecard.metadata.model_name:>30s}    {bold(f'{scorecard[sort_metric_key].result:<15.5}', rich=False)} {metrics}"
+            )
