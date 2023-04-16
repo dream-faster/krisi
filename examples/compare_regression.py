66fcdf25c6ddd03daa052f0b059d8f7f2a231f4b
Daniel Szemerey <account@szemerey.eu>
2023-04-16 14:41:31 +0200

    feature: Updated examples.

--- examples/compare_regression.py
+++ examples/compare_regression.py
@@ -5,14 +5,14 @@ from krisi import compare, score
 scorecards = [
     score(y=np.random.rand(1000), predictions=np.random.rand(1000)) for _ in range(5)
 ]
-print(compare(scorecards, sort_metric_key="rmse", metrics_to_display=["mse", "mae"]))
+print(compare(scorecards, metrics_to_display=["rmse", "mse", "mae"]))
 print(
     compare(
         scorecards,
-        sort_metric_key="rmse",
-        metrics_to_display=["mse", "mae"],
+        metrics_to_display=["rmse", "mse", "mae"],
         dataframe=False,
     ),
 )
-print(compare(scorecards, sort_metric_key="rmse"))
+print(compare(scorecards, sort_metric_key="mae"))
+print(compare(scorecards, ["mse", "rmse"]))
 print(compare(scorecards, ["rmse", "mse"]))
