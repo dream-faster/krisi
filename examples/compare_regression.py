47d79b9818d8cb66f786cc733dc450cc49c0b014
Daniel Szemerey <account@szemerey.eu>
2023-04-17 10:53:19 +0200

    chore(Report): Cleaned up argument names of `compare` and added docstrings. (#132)
    * chore(Report): Renamed sort_metric_key to sort_by, better name.
    
    * chore: Renamed metrics_to_dispaly to metric_keys.
    
    * feature: Added docstrings to `compare` function.

--- examples/compare_regression.py
+++ examples/compare_regression.py
@@ -5,14 +5,14 @@ from krisi import compare, score
 scorecards = [
     score(y=np.random.rand(1000), predictions=np.random.rand(1000)) for _ in range(5)
 ]
-print(compare(scorecards, metrics_to_display=["rmse", "mse", "mae"]))
+print(compare(scorecards, metric_keys=["rmse", "mse", "mae"]))
 print(
     compare(
         scorecards,
-        metrics_to_display=["rmse", "mse", "mae"],
+        metric_keys=["rmse", "mse", "mae"],
         dataframe=False,
     ),
 )
-print(compare(scorecards, sort_metric_key="mae"))
+print(compare(scorecards, sort_by="mae"))
 print(compare(scorecards, ["mse", "rmse"]))
 print(compare(scorecards, ["rmse", "mse"]))
--- src/krisi/evaluate/compare.py
+++ src/krisi/evaluate/compare.py
@@ -7,38 +7,62 @@ from krisi.utils.printing import bold
 
 
 def __handle_empty_metrics_to_display(
-    scorecard: ScoreCard, sort_metric_key: Optional[str], metrics_to_display: List[str]
+    scorecard: ScoreCard, sort_by: Optional[str], metric_keys: Optional[List[str]]
 ) -> Tuple[List[str], Optional[str]]:
-    if len(metrics_to_display) == 0:
-        metrics_to_display = [
+    if metric_keys is None:
+        metric_keys = [
             metric.key
             for metric in scorecard.get_all_metrics(only_evaluated=True)
             if isinstance(metric.result, (float, int))
         ]
 
-    if sort_metric_key is not None:
-        if sort_metric_key not in metrics_to_display:
-            metrics_to_display.insert(0, sort_metric_key)
+    if sort_by is not None:
+        if sort_by not in metric_keys:
+            metric_keys.insert(0, sort_by)
         else:
-            metrics_to_display.remove(sort_metric_key)
-            metrics_to_display.insert(0, sort_metric_key)
+            metric_keys.remove(sort_by)
+            metric_keys.insert(0, sort_by)
 
     else:
-        sort_metric_key = metrics_to_display[0]
-    return metrics_to_display, sort_metric_key
+        sort_by = metric_keys[0]
+    return metric_keys, sort_by
 
 
 def compare(
     scorecards: List[ScoreCard],
-    metrics_to_display: List[str] = [],
-    sort_metric_key: Optional[str] = None,
+    metric_keys: Optional[List[str]] = None,
+    sort_by: Optional[str] = None,
     dataframe: bool = True,
 ) -> Union[pd.DataFrame, str]:
-    metrics_to_display, sort_metric_key = __handle_empty_metrics_to_display(
-        scorecards[0], sort_metric_key, metrics_to_display
+    """Creates a table where each column is a metric and each row is
+    a scorecard and its corresponding results.
+
+    Parameters
+    ----------
+    scorecards : List[ScoreCard]
+        ScoreCards to compare.
+    metric_keys : Optional[List[str]], optional
+        List of metrics to dispaly. If not set it will return all
+        evaluated metrics on the first scorecard.
+        Sorts the results by the first element of this list if `sort_by` is not specified., by default None
+    sort_by : Optional[str], optional
+        `Metric` to sort results by. Selected `Metric` will be displayed in the first row.
+        If not specified metrics will be sorted by the first element of `metric_keys`.
+        If `metric_keys` is not specified it will default to the first metric found on the first `ScoreCard`, by default None
+    dataframe : bool, optional
+        Whether it should return a `pd.DataFrame` or a `str`, by default True
+
+    Returns
+    -------
+    Union[pd.DataFrame, str]
+        A comparison table, either in `pd.DataFrame` or `string` format.
+    """
+
+    metric_keys, sort_by = __handle_empty_metrics_to_display(
+        scorecards[0], sort_by, metric_keys
     )
 
-    scorecards.sort(reverse=True, key=lambda x: x[sort_metric_key].result)
+    scorecards.sort(reverse=True, key=lambda x: x[sort_by].result)
     if dataframe:
         return pd.concat(
             [
@@ -47,21 +71,21 @@ def compare(
                     name=metric_key,
                     index=[scorecard.metadata.model_name for scorecard in scorecards],
                 )
-                for metric_key in metrics_to_display
+                for metric_key in metric_keys
             ],
             axis="columns",
         )
     else:
-        metrics_to_display.remove(sort_metric_key)
+        metric_keys.remove(sort_by)
         string_to_return = ""
-        metric_title = "".join([f"{metric:<10s}" for metric in metrics_to_display])
-        string_to_return += f"{'model_name':>30s}    {bold(f'{sort_metric_key:<10s}', rich=False)} {metric_title}"
+        metric_title = "".join([f"{metric:<10s}" for metric in metric_keys])
+        string_to_return += f"{'model_name':>30s}    {bold(f'{sort_by:<10s}', rich=False)} {metric_title}"
 
         for scorecard in scorecards:
             metrics = "".join(
-                [f"{scorecard[metric].result:<10.5}" for metric in metrics_to_display]
+                [f"{scorecard[metric].result:<10.5}" for metric in metric_keys]
             )
 
-            string_to_return += f"\n{scorecard.metadata.model_name:>30s}    {bold(f'{scorecard[sort_metric_key].result:<10.5}', rich=False)} {metrics}"
+            string_to_return += f"\n{scorecard.metadata.model_name:>30s}    {bold(f'{scorecard[sort_by].result:<10.5}', rich=False)} {metrics}"
 
         return string_to_return
