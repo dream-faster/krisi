0683fa6d2bbca348536384ba9a128936b886ea27
Daniel Szemerey <account@szemerey.eu>
2023-04-17 10:48:50 +0200

    feature: Added docstrings to `compare` function.

--- src/krisi/evaluate/compare.py
+++ src/krisi/evaluate/compare.py
@@ -7,9 +7,9 @@ from krisi.utils.printing import bold
 
 
 def __handle_empty_metrics_to_display(
-    scorecard: ScoreCard, sort_by: Optional[str], metric_keys: List[str]
+    scorecard: ScoreCard, sort_by: Optional[str], metric_keys: Optional[List[str]]
 ) -> Tuple[List[str], Optional[str]]:
-    if len(metric_keys) == 0:
+    if metric_keys is None:
         metric_keys = [
             metric.key
             for metric in scorecard.get_all_metrics(only_evaluated=True)
@@ -30,10 +30,34 @@ def __handle_empty_metrics_to_display(
 
 def compare(
     scorecards: List[ScoreCard],
-    metric_keys: List[str] = [],
+    metric_keys: Optional[List[str]] = None,
     sort_by: Optional[str] = None,
     dataframe: bool = True,
 ) -> Union[pd.DataFrame, str]:
+    """Creates a table where each column is a metric and each row is
+    a scorecard and its corresponding results.
+
+    Parameters
+    ----------
+    scorecards : List[ScoreCard]
+        ScoreCards to compare.
+    metric_keys : Optional[List[str]], optional
+        List of metrics to dispaly. If not set it will return all
+        evaluated metrics on the first scorecard.
+        Sorts the results by the first element of this list if `sort_by` is not specified., by default None
+    sort_by : Optional[str], optional
+        `Metric` to sort results by. Selected `Metric` will be displayed in the first row.
+        If not specified metrics will be sorted by the first element of `metric_keys`.
+        If `metric_keys` is not specified it will default to the first metric found on the first `ScoreCard`, by default None
+    dataframe : bool, optional
+        Whether it should return a `pd.DataFrame` or a `str`, by default True
+
+    Returns
+    -------
+    Union[pd.DataFrame, str]
+        A comparison table, either in `pd.DataFrame` or `string` format.
+    """
+
     metric_keys, sort_by = __handle_empty_metrics_to_display(
         scorecards[0], sort_by, metric_keys
     )
