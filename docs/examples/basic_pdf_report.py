dbad319312153c560949ea5616bc3d2f5f74a3e9
Daniel Szemerey <account@szemerey.eu>
2023-04-19 16:53:44 +0200

    fix: Import.

--- docs/examples/basic_pdf_report.py
+++ docs/examples/basic_pdf_report.py
@@ -4,7 +4,7 @@ Basic PDF report
 """
 # mkdocs_gallery_thumbnail_path = 'images/example_thumnail.png'
 
-from examples.basic_report_rolling import basic_report_rolling
+from docs.examples.basic_report_rolling import basic_report_rolling
 
 insample_report, outsample_report = basic_report_rolling()
 outsample_report.generate_report(display_modes="pdf")
