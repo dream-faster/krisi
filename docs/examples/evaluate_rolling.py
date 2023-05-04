6fabae26a085ee6beb9d874c6f29db10f5a2ba8e
Daniel Szemerey <account@szemerey.eu>
2023-05-04 17:28:40 +0200

    feature: Upgraded example to cover `Calculation.both`.

--- docs/examples/evaluate_rolling.py
+++ docs/examples/evaluate_rolling.py
@@ -10,10 +10,15 @@ from krisi import score
 from krisi.evaluate.type import Calculation
 
 datasize = 1000
-print(
-    score(
-        y=np.random.rand(datasize),
-        predictions=np.random.rand(datasize),
-        calculation=Calculation.both,
-    )
+score(
+    y=np.random.rand(datasize),
+    predictions=np.random.rand(datasize),
+    calculation=Calculation.rolling,
+).print()
+
+
+score(
+    y=np.random.rand(datasize),
+    predictions=np.random.rand(datasize),
+    calculation=Calculation.both,
 )
