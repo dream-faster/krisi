fix: Calculation.both was not working, as metrics got spread into the scorecard hence they were evaluated twice. Added a boolean to control if a metric was spread from a group or not.

docs/examples/evaluate_rolling.py
@@ -10,10 +10,10 @@ from krisi import score
 from krisi.evaluate.type import Calculation
 
 datasize = 1000
-print(
-    score(
-        y=np.random.rand(datasize),
-        predictions=np.random.rand(datasize),
-        calculation=Calculation.both,
-    )
-)
+# print(
+score(
+    y=np.random.rand(datasize),
+    predictions=np.random.rand(datasize),
+    calculation=Calculation.both,
+).generate_report()
+# )