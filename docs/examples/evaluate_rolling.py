773c5d56dbb61cb48e7af790f1522d5e2fd34611
Daniel Szemerey <account@szemerey.eu>
2023-05-04 17:02:43 +0200

    chore: This example doesnt need this many examples.

--- docs/examples/evaluate_rolling.py
+++ docs/examples/evaluate_rolling.py
@@ -9,9 +9,11 @@ import numpy as np
 from krisi import score
 from krisi.evaluate.type import Calculation
 
-datasize = 1000000
-score(
-    y=np.random.rand(datasize),
-    predictions=np.random.rand(datasize),
-    calculation=Calculation.rolling,
+datasize = 1000
+print(
+    score(
+        y=np.random.rand(datasize),
+        predictions=np.random.rand(datasize),
+        calculation=Calculation.both,
+    )
 )
