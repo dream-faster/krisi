777250c6525b5e2c4c7ce8ef82f72ffaf4b9c9ed
Daniel Szemerey <account@szemerey.eu>
2023-05-11 11:28:30 +0200

    feature: We should clean the directory first in tests.

--- tests/unit/test_saving.py
+++ tests/unit/test_saving.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 
 import numpy as np
@@ -9,7 +10,14 @@ from krisi.evaluate.type import PathConst
 
 def test_saving():
     project_name = "Example Load Overwritten 9832423"
-
+    path = Path(
+        os.path.join(
+            PathConst.default_eval_output_path,
+            os.path.join(Path(project_name), PathConst.default_save_output_path),
+        )
+    )
+    if os.path.exists(path):
+        shutil.rmtree(path)
     for _ in range(5):
         score(
             y=np.random.rand(1000),
@@ -18,14 +26,53 @@ def test_saving():
             model_name="Example Model",
         ).save()
 
-    path = Path(
-        os.path.join(
-            PathConst.default_eval_output_path,
-            os.path.join(Path(project_name), PathConst.default_save_output_path),
-        )
-    )
     project_files = os.listdir(path)
 
     assert (
         len(set(project_files)) == 1
     ), "Models should be overwritten, we got more than one models loaded."
+
+    project_name_2 = "Example Load Not Overwritten random model name"
+    path = Path(
+        os.path.join(
+            PathConst.default_eval_output_path,
+            os.path.join(Path(project_name_2), PathConst.default_save_output_path),
+        )
+    )
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    for _ in range(5):
+        score(
+            y=np.random.rand(1000),
+            predictions=np.random.rand(1000),
+            project_name=project_name_2,
+        ).save()
+
+    project_files = os.listdir(path)
+
+    assert (
+        len(set(project_files)) == 5
+    ), f"Models should be not overwritten, we got {len(set(project_files))} models loaded."
+
+    project_name_3 = "Example Load Not Overwritten timestamp boolean"
+    path = Path(
+        os.path.join(
+            PathConst.default_eval_output_path,
+            os.path.join(Path(project_name_3), PathConst.default_save_output_path),
+        )
+    )
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    for _ in range(5):
+        score(
+            y=np.random.rand(1000),
+            predictions=np.random.rand(1000),
+            project_name=project_name_3,
+            model_name="Example Model",
+        ).save(timestamp_no_overwrite=True)
+
+    project_files = os.listdir(path)
+
+    assert (
+        len(set(project_files)) == 5
+    ), "Models should be not overwritten, we set saving to timestamp."
