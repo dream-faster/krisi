5bcd1aa5eb386f5ef7024ec9678a4c85e488dd1c
Daniel Szemerey <account@szemerey.eu>
2023-06-05 18:01:22 +0200

    feature: Naming of the derived metric.

--- src/krisi/evaluate/group.py
+++ src/krisi/evaluate/group.py
@@ -29,7 +29,9 @@ class Group(Metric, Generic[MetricResult]):
         self.metrics = deepcopy(metrics)
 
         for metric in self.metrics:
-            metric.key = f"{self.key}_{metric.key}"
+            metric.key = f"{metric.key}_{self.key}"
+        if isinstance(postprocess_func, Metric):
+            postprocess_func.key = f"{self.key}_{postprocess_func.key}"
 
     def _preprocess(
         self,
@@ -54,10 +56,22 @@ class Group(Metric, Generic[MetricResult]):
     def _postprocess(self, all_metrics: List[Metric], rolling: bool) -> List[Metric]:
         if self.postprocess_func is not None:
             if isinstance(self.postprocess_func, Metric):
-                all_metrics = [
-                    self.postprocess_func._evaluation(
+
+                def deepcopy_and_evaluate(
+                    postprocess_func: Metric, metric: Metric
+                ) -> Metric:
+                    postprocess_metric = deepcopy(postprocess_func)
+                    postprocess_metric.key = f"{postprocess_metric.key}_{metric.key}"
+                    postprocess_metric.name = (
+                        f"{postprocess_metric.name} of {metric.name}"
+                    )
+                    postprocess_metric._evaluation(
                         metric.result_rolling if rolling else metric.result
                     )
+                    return postprocess_metric
+
+                all_metrics = [
+                    deepcopy_and_evaluate(self.postprocess_func, metric)
                     for metric in all_metrics
                 ]
             else:
