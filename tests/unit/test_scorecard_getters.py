e115e0c4d16a7f81a1cbb2b4dcf673eb2548083a
Daniel Szemerey <account@szemerey.eu>
2023-10-03 21:23:23 +0200

    feature: Tests

--- tests/unit/test_scorecard_getters.py
+++ tests/unit/test_scorecard_getters.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pytest
 
 from krisi.evaluate import score
 from krisi.evaluate.group import Group
@@ -23,3 +24,24 @@ def test_spreading_comparions_results():
     metrics = sc.get_all_metrics(spread_comparisons=True)
 
     assert "f_one_score_macro_benchmarking-Δ NS" in [metric.key for metric in metrics]
+
+
+def test_getting_no_skill_metric():
+    groupped_metric = Group[pd.Series](
+        name="benchmarking",
+        key="benchmarking",
+        metrics=[f_one_score_macro],
+        postprocess_funcs=[model_benchmarking(RandomClassifier())],
+    )
+    sc = score(
+        pd.Series(np.random.randint(100)),
+        pd.Series(np.random.randint(100)),
+        default_metrics=groupped_metric,
+    )
+    sc.evaluate()
+    metric = sc["f_one_score_macro_benchmarking-Δ NS"]
+
+    assert "f_one_score_macro_benchmarking-Δ NS" == metric.key
+
+    with pytest.raises(AssertionError):
+        metric = sc["f_one_score_macro_benchmarking-Δ NS-too-many-dashes"]
