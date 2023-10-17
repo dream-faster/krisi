452cac722f7a4411dd248b11daa6fa0716b41618
Daniel Szemerey <account@szemerey.eu>
2023-10-17 12:57:32 +0200

    fix(Benchmarking): Random model now receives `X` and random chunking works.

--- tests/test_benchmarking.py
+++ tests/test_benchmarking.py
@@ -5,6 +5,7 @@ from krisi.evaluate.group import Group
 from krisi.evaluate.library.benchmarking import (
     PerfectModel,
     RandomClassifier,
+    RandomClassifierChunked,
     WorstModel,
     model_benchmarking,
 )
@@ -26,6 +27,29 @@ def test_benchmarking_random():
         key="benchmarking",
         metrics=[f_one_score_macro],
         postprocess_funcs=[model_benchmarking(RandomClassifier())],
+    )
+    X, y = generate_synthetic_data(task=Task.classification, num_obs=1000)
+    sample_weight = pd.Series([1.0] * len(y))
+    preds_probs = generate_synthetic_predictions_binary(y, sample_weight)
+    predictions = preds_probs.iloc[:, 0]
+    probabilities = preds_probs.iloc[:, 1:3]
+
+    sc = score(
+        y,
+        predictions,
+        probabilities,
+        sample_weight=sample_weight,
+        default_metrics=[groupped_metric],
+    )
+    sc.print()
+
+
+def test_benchmarking_random_chunked():
+    groupped_metric = Group[pd.Series](
+        name="benchmarking",
+        key="benchmarking",
+        metrics=[f_one_score_macro],
+        postprocess_funcs=[model_benchmarking(RandomClassifierChunked(5))],
     )
     X, y = generate_synthetic_data(task=Task.classification, num_obs=1000)
     sample_weight = pd.Series([1.0] * len(y))
