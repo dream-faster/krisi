61bf9f7ce4026a2c2ea646ef666891edcfd5c850
Daniel Szemerey <account@szemerey.eu>
2023-11-20 12:05:24 +0100

    fix: Tests

--- tests/unit/test_scorecard_getters.py
+++ tests/unit/test_scorecard_getters.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pytest
 
 from krisi.evaluate import score
+from krisi.evaluate.library.benchmarking import RandomClassifierChunked
 from krisi.evaluate.library.default_metrics_classification import f_one_score_macro
 
 
@@ -11,11 +12,13 @@ def test_spreading_comparions_results():
         pd.Series(np.random.randint(2, size=100)),
         pd.Series(np.random.randint(2, size=100)),
         default_metrics=[f_one_score_macro],
+        calculation=["benchmark"],
+        benchmark_models=RandomClassifierChunked(0.05),
     )
-    sc.evaluate()
+
     metrics = sc.get_all_metrics(spread_comparisons=True)
 
-    assert "f_one_score_macro_benchmarking-Δ NS" in [metric.key for metric in metrics]
+    assert "f_one_score_macro-Δ NS" in [metric.key for metric in metrics]
 
 
 def test_getting_no_skill_metric():
@@ -23,11 +26,13 @@ def test_getting_no_skill_metric():
         pd.Series(np.random.randint(2, size=100)),
         pd.Series(np.random.randint(2, size=100)),
         default_metrics=[f_one_score_macro],
+        calculation=["benchmark"],
+        benchmark_models=RandomClassifierChunked(0.05),
     )
-    sc.evaluate()
-    metric = sc["f_one_score_macro_benchmarking-Δ NS"]
 
-    assert "f_one_score_macro_benchmarking-Δ NS" == metric.key
+    metric = sc["f_one_score_macro-Δ NS"]
+
+    assert "f_one_score_macro-Δ NS" == metric.key
 
     with pytest.raises(AssertionError):
-        metric = sc["f_one_score_macro_benchmarking-Δ NS-too-many-dashes"]
+        metric = sc["f_one_score_macro-Δ NS-too-many-dashes"]
